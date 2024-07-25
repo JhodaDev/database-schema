0923d043f6991cff6d33345a9736ba8b5e2b4999
JhodaDev <rojasjhonatan324@gmail.com>
2024-07-25 14:21:04 -0500

    feat: se agrega tooltips a opciones del menu

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -3,10 +3,11 @@ import { VscJson } from "react-icons/vsc";
 import { JsonGenerator } from "./JsonGenerator";
 import navigateStore from "../store/navigateStore";
 import { MarkdownComponente } from "./Markdown";
+import { Tooltip } from "react-tooltip";
 
 const menu = [
   { component: MarkdownComponente, icon: <IoMdCode />, label: "Markdown" },
-  { component: JsonGenerator, icon: <VscJson />, label: "Json" },
+  { component: JsonGenerator, icon: <VscJson />, label: "Generate data" },
 ];
 
 export const LeftBar = () => {
@@ -19,9 +20,24 @@ export const LeftBar = () => {
   return (
     <div className="h-screen bg-[#1f1f1f] flex flex-col items-center text-white text-xl py-4 gap-6">
       {menu.map((item) => (
-        <button key={item.label} onClick={() => handleClick(item.component)}>
-          {item.icon}
-        </button>
+        <div key={crypto.randomUUID()}>
+          <button
+            data-tooltip-content={item.label}
+            data-tooltip-id={item.label}
+            key={item.label}
+            onClick={() => handleClick(item.component)}
+          >
+            {item.icon}
+          </button>
+          <Tooltip
+            id={item.label}
+            place="right"
+            effect="solid"
+            delayShow={500}
+            variant="dark"
+            className="z-50"
+          />
+        </div>
       ))}
     </div>
   );
