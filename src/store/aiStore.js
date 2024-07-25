3a96c24c8fbc5e552fbfc9ee83c23ec76433c3dd
JhodaDev <rojasjhonatan324@gmail.com>
2024-07-25 14:20:08 -0500

    refactor: se remueve el estado de la tabla

--- src/store/aiStore.js
+++ src/store/aiStore.js
@@ -4,8 +4,6 @@ import { create } from "zustand";
 const useAIStore = create((set) => ({
     resultSchema: null,
     setResultSchema: (resultSchema) => set({ resultSchema }),
-    tables: [],
-    setTables: (tables) => set({ tables }),
     markdown: "",
     setMarkdown: (markdown) => set({ markdown }),
 }))
