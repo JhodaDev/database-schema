f3882ec9d95e3ac36203453824dd0f31eb866c90
JhodaDev <rojasjhonatan324@gmail.com>
2024-07-25 14:20:19 -0500

    feat: se agrega nuevo estados de nodos

--- src/store/tableStore.js
+++ src/store/tableStore.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 const tableStore = create((set) => ({
     activeIndex: null,
     setActiveIndex: (index) => set({ activeIndex: index }),
+    nodes: [],
+    setNodes: (nodes) => set({ nodes }),
 }))
 
 export default tableStore;
