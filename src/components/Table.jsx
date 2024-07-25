0639d452a1fd7b4fe57c46dbbfff99ec9296c1fe
JhodaDev <rojasjhonatan324@gmail.com>
2024-07-25 00:57:40 -0500

    fix: se ajustan estilos

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import tableStore from "../store/tableStore";
+// import tableStore from "../store/tableStore";
 import { Input } from "./Input";
 import PropTypes from "prop-types";
 import { generateUUIDWithoutHyphens } from "../helpers/parseText";
@@ -8,7 +8,7 @@ import { IconAdd } from "./icons/IconAdd";
 
 function Table({ data }) {
   const [items, setItems] = useState(data.fields);
-  const { activeIndex, setActiveIndex } = tableStore();
+  // const { activeIndex, setActiveIndex } = tableStore();
 
   const handleAddField = () => {
     const globalId = generateUUIDWithoutHyphens();
@@ -24,19 +24,19 @@ function Table({ data }) {
       },
     ]);
 
-    setActiveIndex(globalId);
+    // setActiveIndex(globalId);
   };
 
   return (
     <div className="">
-      <div className="w-full text-center rounded-tr-lg rounded-lg bg-[#40CF8F] border-b-transparent px-1 pb-2">
+      <div className="w-full text-center rounded-tr-lg rounded-lg bg-[#40CF8F] border-b-transparent px-1 pb-1">
         <div className="flex px-3 py-2 gap-2">
           <IconCube />
           <span className="font-medium">{data.table}</span>
         </div>
         <div className="w-full bg-white rounded-tl-md rounded-tr-md flex flex-col">
           {items.map((item) => (
-            <div key={item.id} className="flex">
+            <div key={item.id} className="flex hover:bg-gray-100">
               <Input
                 key={`${item.fields[0].id}-left`}
                 id={item.fields[0].id}
@@ -53,7 +53,7 @@ function Table({ data }) {
           ))}
         </div>
         <div
-          className="py-2 bg-white flex items-center justify-center gap-2"
+          className="py-2 bg-white flex items-center justify-center gap-2 rounded-bl-lg rounded-br-lg"
           onClick={handleAddField}
         >
           <IconAdd />
