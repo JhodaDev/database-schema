507195fadcc136f2d00dbfc5fe63563b3891ee23
JhodaDev <rojasjhonatan324@gmail.com>
2024-08-05 22:47:37 -0500

    refactor: Update LeftBar component to use NavLink instead of Link for menu items

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -3,7 +3,7 @@ import { VscJson } from "react-icons/vsc";
 import navigateStore from "../store/navigateStore";
 import { Tooltip } from "react-tooltip";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { IconData } from "./icons/IconData";
 
 const menu = [
@@ -16,32 +16,29 @@ export const LeftBar = () => {
   const [selected, setSelected] = useState(menu[0].label);
   const { setHref } = navigateStore();
 
-  const handleClick = (href, label) => {
-    setHref(href);
-    setSelected(label);
-  };
+  const handleClick = (href, label) => {};
 
   return (
     <div className="h-screen bg-[#1f1f1f] flex flex-col items-center text-white text-xl py-4 gap-3">
       {menu.map((item) => (
         <div
-          className={`
-            w-full flex justify-center items-center h-9 relative ${
-              selected === item.label
-                ? "bg-white text-black rounded-tl-md rounded-bl-md "
-                : ""
-            }`}
+          className="w-full flex justify-center items-center h-9"
           key={crypto.randomUUID()}
         >
-          <Link
+          <NavLink
             to={item.href}
             data-tooltip-content={item.label}
             data-tooltip-id={item.label}
             key={item.label}
             onClick={() => handleClick(item.href, item.label)}
+            className={({ isActive }) =>
+              isActive
+                ? "bg-white text-black rounded-tl-md rounded-bl-md w-full flex justify-center items-center h-9 relative"
+                : "w-full flex justify-center items-center h-9 relative"
+            }
           >
             {item.icon}
-          </Link>
+          </NavLink>
           <Tooltip
             id={item.label}
             place="right"
